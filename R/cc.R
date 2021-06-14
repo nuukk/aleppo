4ba700dd201e920c83a4383412a0037c05dd9e88
nuukk <84548764+nuukk@users.noreply.github.com>
2021-06-14 13:18:55 +0900

    Update cc.R

--- R/cc.R
+++ R/cc.R
@@ -1,5 +1,6 @@
 cc <- function(...,type=c('Y','N'))
 {
+  if(missing(type)) type <- 'N'
   if(type=='Y') {
     write.table(...,file='clipboard',sep="\t",row.names=F,col.names=T)
   } else if(type=='N') {
