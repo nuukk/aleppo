2a82ae76c1dc6085a73cb6e8d13dc579b8dc3faa
nuukk <84548764+nuukk@users.noreply.github.com>
2021-06-29 13:37:40 +0900

    Create upload_table.R

---
+++ R/upload_table.R
@@ -0,0 +1,19 @@
+upload_table <- function(...,name,dbname,dbuser,dbpw,dbhost,port,append=c('Y','N'))
+{
+  x <- data.frame(...)
+  name <- deparse(substitute(name))
+  if(missing(append)) append <- 'Y'
+  a <- dbConnect(dbDriver("MySQL"),
+                 dbname=dbname,
+                 user=dbuser,
+                 password=dbpw,
+                 host=dbhost,
+                 port=port)
+  dbSendQuery(a, 'set character set "euckr"')
+  if(append=='Y') {
+    dbWriteTable(a,name,x,row.names=F,append=T)
+  } else {
+    dbWriteTable(a,name,x,row.names=F,append=F)
+  }
+  dbDisconnect(a)
+}
