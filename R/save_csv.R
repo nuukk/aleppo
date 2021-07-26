1dd84f5f538dc6552a840d21aa51fffd30ba522a
nuukk <84548764+nuukk@users.noreply.github.com>
2021-07-26 10:37:16 +0900

    Update save_csv.R

--- R/save_csv.R
+++ R/save_csv.R
@@ -9,5 +9,10 @@ save_csv <- function(...,filename="")
     data[,(names(data)[dn]):=lapply(.SD,as.Date),.SDcol=names(data)[dn]]
   } 
   dir <- choose.dir(default=getwd(),caption='저장할 위치를 선택하세요')
-  fwrite(data,enc2native(paste0(dir,'/',filename,'.csv')),bom=TRUE)
+  file_dir_name <- enc2native(paste0(dir,'/',filename,'.csv'))
+  if(file.exists(file_dir_name)) {
+    message('같은 이름의 파일이 존재해요. 파일 이름에 생성 시간이 추가됩니다!')
+    file_dir_name <- enc2native(paste0(dir,'/',filename,'_',gsub(' |:','-',paste0(Sys.time())),'.csv'))
+  }
+  fwrite(data,file_dir_name,bom=TRUE)
 }
